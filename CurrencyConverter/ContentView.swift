9c03f995eff0ec00268f73a15677020e44ca5b86
Denis Efremov <denis.efremov@freightera.com>
2024-02-19 18:13:12 -0800

    Initial Commit

---
+++ CurrencyConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyConverter
+//
+//  Created by Denis Efremov on 2024-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Denis Efremov on 2024-02-19.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
